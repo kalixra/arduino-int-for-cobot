b3be5b1e6b78a2e2415619035360c8c9eec40d7d
Kalixra <110695721+kalixra@users.noreply.github.com>
2024-10-30 11:42:18 -0700

    Add files via upload

---
+++ setup_lcd-debug.ino
@@ -0,0 +1,27 @@
+ // RS=12, E=11, D4=5, D5=4, D6=3, D7=2
+// include the library code:
+#include <LiquidCrystal.h>
+
+// Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() 
+{
+	// set up the LCD's number of columns and rows:
+	lcd.begin(16, 2);
+
+	// Clears the LCD screen
+	lcd.clear();
+}
+
+void loop() 
+{
+	// Print a message to the LCD.
+	lcd.print(" Hello world!");
+
+	// set the cursor to column 0, line 1
+	// (note: line 1 is the second row, since counting begins with 0):
+	lcd.setCursor(0, 1);
+	// Print a message to the LCD.
+	lcd.print(" LCD Tutorial");
+}
